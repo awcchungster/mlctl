698891a767180e6cd71316a73faba12669f34c8b
awcchungster <awcchungster@gmail.com>
2021-08-23 20:31:49 +0000

    Add host to pipeline run

--- mlctl/kfp/pipeline.py
+++ mlctl/kfp/pipeline.py
@@ -5,4 +5,4 @@ def run_pipeline(function, host):
     kfp.compiler.Compiler().compile(function, 'mlctl_pipeline.yaml')
 
     # change the host to where you want to submit the job
-    kfp.Client().create_run_from_pipeline_package('mlctl_pipeline.yaml', arguments={})
+    kfp.Client(host=host).create_run_from_pipeline_package('mlctl_pipeline.yaml', arguments={})
